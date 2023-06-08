401ef076789f0254cf1d3096067ef5dd62c1a087
irahayuningtias <2031710085@student.polinema.ac.id>
2023-06-08 20:12:39 +0700

    navbar logout

--- kriteria.php
+++ kriteria.php
@@ -50,36 +50,16 @@
               				<i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Matriks Alternatif</span>
             			</a>
 					</li>
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="logout.php">
+              				<i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Logout</span>
+            			</a>
+					</li>
 				</ul>
 			</div>
 		</nav>
 
 		<div class="main">
-			<nav class="navbar navbar-expand navbar-light navbar-bg">
-				<a class="sidebar-toggle js-sidebar-toggle">
-          			<i class="hamburger align-self-center"></i>
-        		</a>
-				<div class="navbar-collapse collapse">
-					<ul class="navbar-nav navbar-align">
-						<li class="nav-item dropdown">
-							<a class="nav-icon dropdown-toggle d-inline-block d-sm-none" href="#" data-bs-toggle="dropdown">
-                				<i class="align-middle" data-feather="settings"></i>
-              				</a>
-							<a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-                				<span class="text-dark">Admin</span>
-              				</a>
-							<div class="dropdown-menu dropdown-menu-end">
-								<a class="dropdown-item" href="adminkit-dev/static/pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
-								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="index.php"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>
-								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="login.php">Log out</a>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</nav>
-
 			<main class="content">
 				<div class="container-fluid p-0">
 					<h1 class="h3 mb-3">Data <strong>Alternatif</strong></h1>
